638b2aca51473ae241b2cfef1f6ac16671e5ebd4
arafatsau <arafatsau78@gmail.com>
2021-10-07 16:06:59 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// grab everything we need
+const crazyButtons = document.querySelectorAll('.btn-crazy');
+
+// define our functions
+function goCrazy() {
+  // get a random number for the left offset
+  // random num for top offset
+  const offsetLeft = Math.random() * (window.innerWidth - this.clientWidth);
+  const offsetTop  = Math.random() * (window.innerHeight - this.clientHeight);
+
+  console.log(offsetLeft, offsetTop);
+
+  // apply those numbers to the button
+  this.style.top = offsetTop + 'px';
+  this.style.left = offsetLeft + 'px';
+}
+
+// add event listeners
+crazyButtons.forEach(button => button.addEventListener('mouseenter', goCrazy));
